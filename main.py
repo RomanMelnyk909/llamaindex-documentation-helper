451d5289c6f7f949ad48f8ee8d84c84199901035
Roman_Melnyk_909 <pvrrivne@gmail.com>
2025-04-22 12:01:17 +0300

    connected with streamlit

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 from dotenv import load_dotenv
 import os
 from pinecone import Pinecone
-from llama_index.core import VectorStoreIndex, ServiceContext, Settings
+from llama_index.core import VectorStoreIndex, Settings
 from llama_index.vector_stores.pinecone import PineconeVectorStore
-from llama_index.core.callbacks import LlamaDebugHandler, CallbackManage
+from llama_index.core.callbacks import LlamaDebugHandler, CallbackManager
+
+import streamlit as st
 
 # This code send query to vactor db with context data
 
 load_dotenv()
 
-pc = Pinecone(api_key=os.environ["PINECONE_API_KEY"])
 
-if __name__ == "__main__":
+@st.cache_resource(show_spinner=False)
+def get_index() -> VectorStoreIndex:
     print("RAG...")
+
+    pc = Pinecone(api_key=os.environ["PINECONE_API_KEY"])
 
     pinecone_index = pc.Index("llamaindex-documentation-helper")
     vector_store = PineconeVectorStore(pinecone_index=pinecone_index)
@@ -20,14 +24,60 @@ if __name__ == "__main__":
     llama_debug = LlamaDebugHandler(print_trace_on_end=True)
     Settings.callback_manager = CallbackManager(handlers=[llama_debug])
 
-    index = VectorStoreIndex.from_vector_store(
-        vector_store=vector_store
+    return VectorStoreIndex.from_vector_store(vector_store=vector_store)
+
+    # query = "What is Llamaindex query engine?"
+    # query_engine = index.as_query_engine()
+    # responce = query_engine.query(query)
+
+    # print(responce)
+
+    # print("File was run successfully....")
+
+
+index = get_index()
+
+if "chat_engine" not in st.session_state.keys():
+    st.session_state.chat_engine = index.as_chat_engine(
+        chat_mode="context", verbose=True
     )
 
-    query = "What is Llamaindex query engine?"
-    query_engine = index.as_query_engine()
-    responce = query_engine.query(query)
+st.set_page_config(
+    page_title="Chat with LlamaIndex docs, powered by LlamaIndex",
+    page_icon="W ",
+    layout="centered",
+    initial_sidebar_state="auto",
+    menu_items=None,
+)
 
-    print(responce)
+st.title("Chat with LlamaIndex docs")
 
-    print("File was run successfully....")
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [
+        {
+            "role": "assistant",
+            "content": "Ask me a question about LlamaIndex open source python library?"
+        }
+    ]
+
+
+if prompt := st.chat_input("Your question"):
+    st.session_state.messages.append({ 
+        "role": "user",
+        "content": prompt
+     })
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            responce = st.session_state.chat_engine.chat(message=prompt)
+            st.write(responce.response)
+            message = {
+                "role": "assistant",
+                "content": responce.response
+            }
+            st.session_state.messages.append(message)
